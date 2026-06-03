9ea234e7ba88799953c91917f4111e20d88c2cc2
EiRuMF <iuriemf@gmail.com>
2026-06-03 16:45:58 +0700

    register

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ email, password }),
+          body: JSON.stringify({ name, email, phone_number, password }),
         },
       );
 
